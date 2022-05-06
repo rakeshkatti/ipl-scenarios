bd92972fee8a95f528e5488cad1553d8cf273a18
Rakesh Katti <rakeshkatti@Rakeshs-MacBook-Air.local>
2022-05-06 23:56:31 +0530

    Adding first changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import "antd/dist/antd.css";
+import "./App.css";
+import { tabularData, matchData } from "./data";
+import React, { useState } from "react";
+import { Game } from "./components/Game";
+
+const TableRow = ({ team, m, w, l, p, nr, nrr }) => (
+  <div className="table-row">
+    <div>{team.toUpperCase()}</div>
+    <div>{m}</div>
+    <div>{w}</div>
+    <div>{l}</div>
+    <div>{nr}</div>
+    <div>{p}</div>
+    <div>{nrr}</div>
+  </div>
+);
 
 function App() {
+  const [table, setTable] = useState(tabularData);
+  const [matches, setMatches] = useState(matchData);
+
+  const selectWinner = (match, winner) => {
+    const selectedMatch = matches[match];
+    const team1 = selectedMatch.t1;
+    const team2 = selectedMatch.t2;
+    const winningTeam = winner;
+    const losingTeam = winner === team1 ? team2 : team1;
+    const winningTeamRow = table[winningTeam];
+    const losingTeamRow = table[losingTeam];
+    if (selectedMatch.win === "") {
+      setTable({
+        ...table,
+        [winningTeam]: {
+          ...winningTeamRow,
+          m: winningTeamRow.m + 1,
+          w: winningTeamRow.w + 1,
+          p: winningTeamRow.p + 2,
+        },
+        [losingTeam]: {
+          ...losingTeamRow,
+          m: losingTeamRow.m + 1,
+          l: losingTeamRow.l + 1,
+        },
+      });
+    } else if (selectedMatch.win !== winner) {
+      setTable({
+        ...table,
+        [winningTeam]: {
+          ...winningTeamRow,
+          w: winningTeamRow.w + 1,
+          p: winningTeamRow.p + 2,
+          l: winningTeamRow.l - 1,
+        },
+        [losingTeam]: {
+          ...losingTeamRow,
+          l: losingTeamRow.l + 1,
+          w: losingTeamRow.w - 1,
+          p: losingTeamRow.p - 2,
+        },
+      });
+    }
+    setMatches({
+      ...matches,
+      [match]: {
+        ...selectedMatch,
+        win: winner,
+      },
+    });
+  };
+
+  const sortedTable = Object.keys(table).sort((t1, t2) => {
+    const a = table[t1];
+    const b = table[t2];
+    if (a.p === b.p) {
+      return parseFloat(b.nrr) - parseFloat(a.nrr);
+    }
+    return b.p - a.p;
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="header">IPL Points Table Scenarios Calculator</h1>
+      <div className="table-header">
+        <div>Team</div>
+        <div>PLD</div>
+        <div>WON</div>
+        <div>LOST</div>
+        <div>N/R</div>
+        <div>PTS</div>
+        <div>NET RR</div>
+      </div>
+      {sortedTable.map((team) => (
+        <TableRow team={team} {...table[team]} />
+      ))}
+      <h3 className="sub-header">
+        Choose the winning team to see the table change according those
+        scenarios
+      </h3>
+      <div className="game-container">
+        {Object.keys(matches).map((match) => (
+          <Game match={match} selectWinner={selectWinner} {...matches[match]} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/Game.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Radio } from "antd";
+
+export const Game = ({ t1, t2, win, match, selectWinner }) => {
+  const onChange = (e) => {
+    selectWinner(match, e.target.value);
+  };
+  return (
+    <div>
+      <Radio.Group onChange={onChange} optionType="button" buttonStyle="solid">
+        <Radio.Button value={t1}>{t1.toUpperCase()}</Radio.Button>
+        <Radio.Button value={t2}>{t2.toUpperCase()}</Radio.Button>
+      </Radio.Group>
+    </div>
+  );
+};
