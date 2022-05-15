5dbc2a492e8b1f515fea5236e1b5d59bd2328733
Rakesh Katti <rakesh.katti@gmail.com>
2022-05-15 19:19:34 +0530

    No cacha

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   useEffect(() => {
     fetch(
       "https://raw.githubusercontent.com/alexsanjoseph/ipl-scenario-builder/main/data/current_standings.json",
-      { cache: "reload" }
+      { cache: "no-store" }
     )
       .then((resp) => resp.json())
       .then((data) => {
@@ -30,7 +30,7 @@ function App() {
   useEffect(() => {
     fetch(
       "https://raw.githubusercontent.com/alexsanjoseph/ipl-scenario-builder/main/data/filtered_fixtures.json",
-      { cache: "reload" }
+      { cache: "no-store" }
     )
       .then((resp) => resp.json())
       .then((data) => {
