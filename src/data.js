27711973a5d41f17f3574a6292abd1df81004f17
Rakesh Katti <rakesh.katti@gmail.com>
2024-05-10 23:52:04 +0530

    after gt

--- src/data.js
+++ src/data.js
@@ -4,15 +4,14 @@ export const tabularData = {
 	srh: { m: 12, w: 7, l: 5, nr: 0, nrr: 0.406, p: 14 },
 	lsg: { m: 12, w: 6, l: 6, nr: 0, nrr: -0.769, p: 12 },
 	dc: { m: 12, w: 6, l: 6, nr: 0, nrr: -0.316, p: 12 },
-	csk: { m: 11, w: 6, l: 5, nr: 0, nrr: 0.7, p: 12 },
-	gt: { m: 11, w: 4, l: 7, nr: 0, nrr: -1.32, p: 8 },
+	csk: { m: 12, w: 6, l: 6, nr: 0, nrr: 0.491, p: 12 },
+	gt: { m: 12, w: 5, l: 7, nr: 0, nrr: -1.063, p: 10 },
 	pbks: { m: 12, w: 4, l: 8, nr: 0, nrr: -0.423, p: 8 },
 	mi: { m: 12, w: 4, l: 8, nr: 0, nrr: -0.212, p: 8 },
 	rcb: { m: 12, w: 5, l: 7, nr: 0, nrr: 0.217, p: 10 },
 };
 
 export const matchData = {
-	59: { t1: "gt", t2: "csk", win: "" },
 	60: { t1: "kkr", t2: "mi", win: "" },
 	61: { t1: "csk", t2: "rr", win: "" },
 	62: { t1: "rcb", t2: "dc", win: "" },
